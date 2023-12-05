53d2a306f023c60573637ab03fcd220eb062dbdd
matheusrodrigues1 <matech00@outlook.com>
2023-12-05 13:55:30 -0300

    products list page 1

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,22 @@
+import { Product } from "@prisma/client"
+import Link from "next/link";
+import PriceTag from "./PriceTag";
+
+
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({product}: ProductCardProps) {
+  return(
+    <Link href={"/products/" + product.id} className="card w-full bg-base-100 hover:shadow-xl transition-shadow">
+      <div className="card-body">
+        <h2 className="card-title">
+          {product.name}
+        </h2>
+        <p>{product.description}</p>
+        <PriceTag price={product.price}/>
+      </div>
+    </Link>
+  )
+}
