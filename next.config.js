a99d9ee6ad71f1d26dc354ebba8e70a5ef595c99
matheusrodrigues1 <matech00@outlook.com>
2023-12-06 12:47:02 -0300

    product list page 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "images.unsplash.com" }],
+    remotePatterns: [{ hostname: "images.unsplash.com" }, { hostname: "plus.unsplash.com" }],
   },
   experimental: {
     serverActions: true,
@@ -9,3 +9,4 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
