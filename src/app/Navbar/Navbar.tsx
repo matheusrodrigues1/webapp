6c3141ab9c0476e483852eff8137d1e1382a3315
matheusrodrigues1 <matech00@outlook.com>
2023-12-13 20:08:13 -0300

    Navbar + Footer /1

---
+++ src/app/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "@/assets/logo.png"
+
+export default function Navbar() {
+  return(
+    <div className="bg-base-100">
+      <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
+        <div className="flex-1">
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            <Image src={Logo} height={40} width={40} alt="Flowmazon logo"/>
+            Flowmazon
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
