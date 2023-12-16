c826e8a3e2563e638ac3efffbf9697d95968e048
matheusrodrigues1 <matech00@outlook.com>
2023-12-16 19:13:50 -0300

    Navbar + Footer /2

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,8 +1,22 @@
 import Image from "next/image"
 import Link from "next/link"
 import Logo from "@/assets/logo.png"
+import { redirect } from "next/navigation";
+import { getCart } from "@/lib/db/cart";
+import ShoppingCartButton from "./ShoppingCartButton";
 
-export default function Navbar() {
+async function searchProducts(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+
+  if(searchQuery) {
+    redirect("/search?query=" + searchQuery)
+  }
+}
+
+export default async function Navbar() {
+  const cart = await getCart();
   return(
     <div className="bg-base-100">
       <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
@@ -11,6 +25,18 @@ export default function Navbar() {
             <Image src={Logo} height={40} width={40} alt="Flowmazon logo"/>
             Flowmazon
           </Link>
+        </div>
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                placeholder="Search"
+                className="input input-bordered w-full min-w-[100px]"
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart}/>
         </div>
       </div>
     </div>
