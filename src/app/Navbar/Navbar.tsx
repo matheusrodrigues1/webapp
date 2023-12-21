b05e7968b0d4b553a3dcf57bcc1e27d8d8d13adf
matheusrodrigues1 <matech00@outlook.com>
2023-12-21 20:53:32 -0300

    User login (Next-Auth) /2

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -4,6 +4,9 @@ import Logo from "@/assets/logo.png"
 import { redirect } from "next/navigation";
 import { getCart } from "@/lib/db/cart";
 import ShoppingCartButton from "./ShoppingCartButton";
+import UserMenuButton from "./UserMenuButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
 async function searchProducts(formData: FormData) {
   "use server";
@@ -16,7 +19,9 @@ async function searchProducts(formData: FormData) {
 }
 
 export default async function Navbar() {
+  const session = await getServerSession(authOptions)
   const cart = await getCart();
+
   return(
     <div className="bg-base-100">
       <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
@@ -37,6 +42,7 @@ export default async function Navbar() {
             </div>
           </form>
           <ShoppingCartButton cart={cart}/>
+          <UserMenuButton session={session}/>
         </div>
       </div>
     </div>
