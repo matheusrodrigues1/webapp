da4f09b818e56ea7c02174cbbb59193cb37d0654
matheusrodrigues1 <matech00@outlook.com>
2023-12-22 19:29:09 -0300

    Merging user cart with anonymous cart /1

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/dist/client/components/headers";
 import { prisma } from "./prisma";
 import { Cart, Prisma } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export type CartWithProducts = Prisma.CartGetPayload<{
   include: {items: {include: {product: true}}}
@@ -16,13 +18,24 @@ export type ShoppingCart = CartWithProducts & {
 }
 
 export async function getCart(): Promise<ShoppingCart | null> {
-  const localCartId = cookies().get("localCartId")?.value
-  const cart = localCartId ?
-  await prisma.cart.findUnique({
-    where: {id: localCartId},
-    include: {items: {include: {product: true}}}
-  })
-  : null;
+  const session = await getServerSession(authOptions);
+
+  let cart: CartWithProducts | null = null;
+
+  if(session) {
+    cart = await prisma.cart.findFirst({
+      where: {userId : session.user.id},
+      include: {items: {include: {product: true}}}
+    })
+  } else {
+    const localCartId = cookies().get("localCartId")?.value
+    cart = localCartId ?
+    await prisma.cart.findUnique({
+      where: {id: localCartId},
+      include: {items: {include: {product: true}}}
+    })
+    : null;
+  }
 
   if(!cart){
     return null;
@@ -39,11 +52,20 @@ export async function getCart(): Promise<ShoppingCart | null> {
 }
 
 export async function createCart(): Promise<ShoppingCart> {
-  const newCart = await prisma.cart.create({
-    data: {}
-  })
+  const session = await getServerSession(authOptions);
 
-  cookies().set("localCartId", newCart.id);
+  let newCart: Cart;
+  if(session) {
+    newCart = await prisma.cart.create({
+      data: {userId: session.user.id}
+    })
+  } else {
+    newCart = await prisma.cart.create({
+      data: {}
+    })
+    cookies().set("localCartId", newCart.id);
+  }
+
 
   return {
     ...newCart,
